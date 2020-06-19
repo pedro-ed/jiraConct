78a07aa480db71bf4f09d1b5a25235148a906dff
Pedro Eduardo <pedroadv991@gmail.com>
2020-06-19 11:24:12 -0300

    Ajuste e criação do metodo post

--- src/controllers/ongsControllers.js
+++ src/controllers/ongsControllers.js
@@ -12,14 +12,25 @@ module.exports = {
     const { name, email, whatsapp, city, uf } = request.body
     const id = crypto.randomBytes(4).toString('HEX')
 
-    await connection('ongs').insert({
-      id,
-      name,
-      email,
-      whatsapp,
-      city,
-      uf,
-    })
+    var xhr = new XMLHttpRequest();
+    var dados = {
+      teste:"isso mesmo"
+    }
+    var url = ""
+    xhr.open("POST",url);
+    request.setRequestHeader('Content-Type', 'application/json');
+    xhr.addEventListener("load", function() {
+    var erroAjax = document.querySelector("#erro-ajax");
+
+    if (xhr.status == 200) {
+        
+    } else {
+    
+    }
+});
+
+xhr.send(dados);
+    
     //Retorno de resposta
     return response.json({ id })
   },
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const routes = require('./routes')
 
+
+
 const app = express()
 app.use(express.json())
 app.use(routes)
