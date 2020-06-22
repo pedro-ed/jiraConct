a2553c7b1f08f0de6d02da5129d079761355e456
pedro-ed <56552391+pedro-ed@users.noreply.github.com>
2020-06-21 22:31:25 -0300

    Delete gitx.bat

--- gitx.bat
+++
@@ -1,7 +0,0 @@
-cd C:\Users\Pedro Eduardo\Documents\Estudos\desenvolvimento\DEV\jiraconnector
-set /p commit="Nome do Commit: "
-git add .
-git status
-git commit -m %commit%
-git push origin master
-set /p input="Commit Feito com Sucesso, Nome: %commit% | Precione Enter para sair"
